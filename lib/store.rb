3e94cd8c1ead65eae583bf1871f8234a64185d23
Payal Dhiman <payaldhiman@outlook.com>
2018-01-17 01:15:42 +0000

    Validations

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,14 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates_length_of :name, minimum: 3
+  validates :annual_revenue, numericality: {only_integer: true, greater_than_or_equal_to: 0}
+  validate :check_apparel
+
+  def check_apparel
+    if !womens_apparel && !mens_apparel
+      errors.add(:womens_apparel, "Store needs to have women's or men's apparel")
+      errors.add(:mens_apparel, "Store needs to have women's or men's apparel")
+    end
+  end
+
 end
